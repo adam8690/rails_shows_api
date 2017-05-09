4dc745cacc607d2ce493a2a6fc76be531f956b6d
Adam <adam8690@hotmail.co.uk>
2017-05-09 20:54:39 +0100

    added join table to database which will map favourite shows to users.

---
+++ app/models/favourite_show.rb
@@ -0,0 +1,2 @@
+class FavouriteShow < ActiveRecord::Base
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170509194741) do
+ActiveRecord::Schema.define(version: 20170509195327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favourite_shows", force: :cascade do |t|
+    t.integer  "show_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "shows", force: :cascade do |t|
     t.string   "title"
