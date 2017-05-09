5fe75b4df118410d0c7c60f8f23a925f5d59032f
Adam <adam8690@hotmail.co.uk>
2017-05-09 20:52:01 +0100

    seeded user table

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,36 +6,37 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Show.delete_all
+User.delete_all
 
-shows = Show.create([{
+s1 = Show.create([{
   "title": "The Great British Bake Off",
   "series": 7,
   "description": "Master bakers Mary & Paul and the incomparable presenting duo of Mel & Sue are back for another interesting series about baking cakes.",
   "image": "placeholder.jpg",
   "programmeID": "b013pqnm" 
 }])
-shows = Show.create([{
+s2 = Show.create([{
   "title": "Arrested Development",
   "series": 1,
   "description": "Level-headed son Michael Bluth takes over family affairs after his father is imprisoned. But the rest of his spoiled, dysfunctional family are making his job unbearable.",
   "image": "http://vignette3.wikia.nocookie.net/arresteddevelopment/images/6/6b/Season_3_square.jpg/revision/latest?cb=20111023065401",
   "programmeID": "b013pqnm" 
 }])
-shows = Show.create([{
+s3 = Show.create([{
   "title": "Rick and Morty",
   "series": 1,
   "description": "An animated series that follows the exploits of a super scientist and his not so bright grandson.",
   "image": "http://static.tvtropes.org/pmwiki/pub/images/ramsignedmailer.jpg",
   "programmeID": "b016pqnm" 
 }])
-shows = Show.create([{
+s4 = Show.create([{
   "title": "It's Always Sunny In Philadelphia",
   "series": 1,
   "description": "Five friends with big egos and slightly arrogant attitudes are the proprietors of an Irish bar in Philadelphia.",
   "image": "http://files.greatermedia.com/uploads/sites/15/2016/06/Sunny.jpg",
   "programmeID": "b017pqnm" 
 }])
-shows = Show.create([{
+s5 = Show.create([{
   "title": "This Morning with Richard Not Judy",
   "series": 1,
   "description": "This Morning With Richard Not Judy or TMWRNJ was a BBC comedy television programme, written by and starring Lee and Herring. Two series were broadcast in 1998 and 1999 on BBC2. The name was a satirical reference to ITV's This Morning which was at the time popularly referred to as This Morning with Richard and Judy.",
@@ -43,3 +44,5 @@ shows = Show.create([{
   "programmeID": "b017pqnm" 
 }])
 
+u1 = User.create(name: "Adam")
+u2 = User.create(name: "Mac")
